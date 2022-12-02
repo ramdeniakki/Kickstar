871475fcda35d2e92c00a49e0d116387698bd08a
ramdeniakki <ramdeniakki35@gmail.com>
2022-12-02 21:38:20 +0530

    start

---
+++ Notes/start.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+// This Stands for Standard input-output stream 
+
+
+using namespace std;
+
+// Standard Library
+
+//  Main Function
+int main()  { 
+
+/*
+    // integer
+    int h =123;
+    
+    // Print the output
+
+    cout<<h<<endl;
+    
+    // character 
+    char ch ='E';
+    cout<<ch<<endl;
+
+    // float 
+
+    float f =1.5;
+    cout<<f<<endl;
+
+    // double 
+    double d =1.23;
+    cout<<d<<endl;
+
+    // sizeof function it declear the how bytes of the variable
+
+     int a=246;
+     cout<<a<<endl;
+
+     int size =sizeof(a);
+     cout<<"The size of a is : " <<size <<endl;
+*/
+
+
+ /* Operators: 
+
+ Airthmetic Operators
+   + ,  -  , * ,  /  
+
+Relational operators 
+=
+>
+<
+>=
+<=
+
+Logical Operator
+&& and 
+|| and
+! not
+ */
+
+// Airthemic operator
+
+int a= 1;
+int b =2;
+
+cout<<a+b<<endl;
+cout<<a-b<<endl;
+cout<<a*b<<endl;
+cout<<a/b<<endl;
+
+cout<<"Next line"<<endl;
+
+// Relational 
+int c =1;
+int d =2;
+
+
+
+
+bool first = (c==d);
+cout<<first<<endl;
+
+ 
+bool second =(c>d);
+cout<<second<<endl;
+
+
+bool third =(c<d);
+cout<<third<<endl;
+
+bool fourth = (c>=d);
+cout<<fourth<<endl;
+
+bool fifth = (c<=d);
+cout<<fifth<<endl;
+
+bool sixth = (a==b);
+cout<<sixth<<endl;
+}
