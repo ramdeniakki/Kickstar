abe9ab275572846fdef4124d2652b90ae62bff6e
ramdeniakki <ramdeniakki35@gmail.com>
2022-12-03 22:28:39 +0530

    start

---
+++ loop.cpp
@@ -0,0 +1,24 @@
+/*
+     Conditional's && loops
+
+
+  => if      --- to specify a block of code to be executed, if a specified condition is true
+ => else     ---to specify a block of code to be executed, if the same condition is false
+ => else     --- if to specify a new condition to test, if the first condition is false
+ => switch   ---to specify many alternative blocks of code to be executed
+
+
+*/
+
+/*
+   cin => used to take user input
+*/
+
+//  if statement
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    cout<<a<<endl;
+}
